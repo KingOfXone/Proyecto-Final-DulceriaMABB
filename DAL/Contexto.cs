48ea012d0fc09f4dcfb93e2fea1cec9c151d4fb2
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2023-11-27 21:59:37 -0400

    Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using DulceriaMABB.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace DulceriaMABB.DAL
+{
+    public class Contexto:DbContext
+    {
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Productos> Productos { get; set; }
+        public DbSet<Recepciones> Recepciones { get; set; }
+        public DbSet<Ventas> Ventas { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+    }
+}
